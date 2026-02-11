1cea2371ca3705cefa6de634f8fbfeb45bcf56c1
Git <tuncayelyazl@gmail.com>
2026-02-11 10:53:32 +0400

    cat activityde deyisiklik eledim

--- app/src/main/java/com/example/evtapsirigi10/CatActivity.kt
+++ app/src/main/java/com/example/evtapsirigi10/CatActivity.kt
@@ -27,7 +27,7 @@ class CatActivity : AppCompatActivity(), SensorEventListener {
         setContentView(R.layout.activity_cat)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left,            systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
       textToSpeech = TextToSpeech(this){status ->
